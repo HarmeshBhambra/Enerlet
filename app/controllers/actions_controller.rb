c19243d448dbbd3188f6baa934450ae6dcb98579
HarmeshBhambra <harmeshbhambra@Harmeshs-MacBook-Air.local>
2016-04-02 20:13:02 -0500

    Changes for outsourcer

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,3 +1,20 @@
 class ActionsController < ApplicationController
 
+def plan_journey
+
+@street_address = "98 Teg Down Meads Winchester, UK"
+url_safe_street_address = URI.encode(@street_address)
+
+url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{url_safe_street_address}"
+
+parsed_data = JSON.parse(open(url).read)
+
+@lat = parsed_data["results"][0]["geometry"]["location"]["lat"]
+
+@lng = parsed_data["results"][0]["geometry"]["location"]["lng"]
+
+render("plan_journey.html.erb")
+
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,6 @@ get "/footprint", :controller => "footprints", :action => "index"
 # Routes for the Actions resource
 get "/action", :controller => "actions", :action => "index"
 
+get "/action/plan_journey", :controller => "actions", :action => "plan_journey"
+
 end
