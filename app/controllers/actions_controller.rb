7b0257be1a486c7393eff778aeca17d85c799a61
HarmeshBhambra <harmeshbhambra@Harmeshs-MacBook-Air.local>
2016-04-17 08:14:17 -0500

    Heroku changes

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -10,14 +10,30 @@ def map
 
   # Origin
   @origin = params[:origin].to_s
-  url_safe_origin = URI.encode(@origin)
+  @url_safe_origin = URI.encode(@origin)
+
+  url_origin = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_origin}"
+
+  parsed_data_origin = JSON.parse(open(url_origin).read)
+
+  @lat_origin = parsed_data_origin["results"][0]["geometry"]["location"]["lat"].to_f
+
+  @lng_origin = parsed_data_origin["results"][0]["geometry"]["location"]["lng"].to_f
 
   # Destination
-  @destination = params[:destination]
-  url_safe_destination = URI.encode(@destination)
+  @destination = params[:destination].to_s
+  @url_safe_destination = URI.encode(@destination)
+
+  url_destination = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_destination}"
+
+  parsed_data_destination = JSON.parse(open(url_destination).read)
+
+  @lat_destination = parsed_data_destination["results"][0]["geometry"]["location"]["lat"].to_f
+
+  @lng_destination = parsed_data_destination["results"][0]["geometry"]["location"]["lng"].to_f
 
   #Google Maps Transit
-  url_transit = "https://maps.googleapis.com/maps/api/directions/json?origin=#{url_safe_origin}&destination=#{url_safe_destination}&mode=transit"
+  url_transit = "https://maps.googleapis.com/maps/api/directions/json?origin=#{@url_safe_origin}&destination=#{@url_safe_destination}&mode=transit"
 
   parsed_data_transit = JSON.parse(open(url_transit).read)
 
@@ -26,7 +42,7 @@ def map
   @carbon_transit = @distance_transit.to_f**0.19
 
   #Google Maps Driving
-  url_driving = "https://maps.googleapis.com/maps/api/directions/json?origin=#{url_safe_origin}&destination=#{url_safe_destination}&mode=driving"
+  url_driving = "https://maps.googleapis.com/maps/api/directions/json?origin=#{@url_safe_origin}&destination=#{@url_safe_destination}&mode=driving"
 
   parsed_data_driving = JSON.parse(open(url_driving).read)
 
@@ -35,7 +51,7 @@ def map
   @carbon_driving = @distance_driving.to_f**0.9042
 
   #Google Maps Walking
-  url_walking = "https://maps.googleapis.com/maps/api/directions/json?origin=#{url_safe_origin}&destination=#{url_safe_destination}&mode=walking"
+  url_walking = "https://maps.googleapis.com/maps/api/directions/json?origin=#{@url_safe_origin}&destination=#{@url_safe_destination}&mode=walking"
 
   parsed_data_walking = JSON.parse(open(url_walking).read)
 
@@ -44,7 +60,7 @@ def map
   @carbon_walking = @distance_walking.to_f*0
 
   #Google Maps Bicycling
-  url_bicycling = "https://maps.googleapis.com/maps/api/directions/json?origin=#{url_safe_origin}&destination=#{url_safe_destination}&mode=bicycling"
+  url_bicycling = "https://maps.googleapis.com/maps/api/directions/json?origin=#{@url_safe_origin}&destination=#{@url_safe_destination}&mode=bicycling"
 
   parsed_data_bicycling = JSON.parse(open(url_bicycling).read)
 
