25137e0e45e6b290e407735d5c6b0cafc5b21103
HarmeshBhambra <harmeshbhambra@wireless-s0-so-150-37-251.uchicago.edu>
2016-04-19 15:29:18 -0500

    Sign In redirect

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,9 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     '/beta' # Or :prefix_to_your_route
   end
+
+  def after_sign_in_path_for(resource)
+    '/beta' # Or :prefix_to_your_route
+  end
+
 end
