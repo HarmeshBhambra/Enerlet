5eba4b3d6cbc8dedfd29c078d7a7f99f0791f4e2
HarmeshBhambra <harmeshbhambra@Harmeshs-MacBook-Air.local>
2016-04-20 20:24:41 -0500

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
 
+  # Routes for the Image resource:
+  # CREATE
+  get "/images/new", :controller => "images", :action => "new"
+  post "/create_image", :controller => "images", :action => "create"
+
+  # READ
+  get "/images", :controller => "images", :action => "index"
+  get "/images/:id", :controller => "images", :action => "show"
+
+  # UPDATE
+  get "/images/:id/edit", :controller => "images", :action => "edit"
+  post "/update_image/:id", :controller => "images", :action => "update"
+
+  # DELETE
+  get "/delete_image/:id", :controller => "images", :action => "destroy"
+  #------------------------------
+
   # Routes for the Survey resource:
   # CREATE
   get "/surveys/new", :controller => "surveys", :action => "new"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420183817) do
+ActiveRecord::Schema.define(version: 20160421011043) do
+
+  create_table "images", force: :cascade do |t|
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "surveys", force: :cascade do |t|
     t.integer  "flight_type"
