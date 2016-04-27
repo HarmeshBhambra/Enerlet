024972dcd920ed3e85a2f832571e99df020763a9
HarmeshBhambra <harmeshbhambra@Harmeshs-MacBook-Air.local>
2016-04-27 17:39:19 -0500

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
 
-  # Routes for the Image resource:
-  # CREATE
-  get "/images/new", :controller => "images", :action => "new"
-  post "/create_image", :controller => "images", :action => "create"
+  # # Routes for the Image resource:
+  # # CREATE
+  # get "/images/new", :controller => "images", :action => "new"
+  # post "/create_image", :controller => "images", :action => "create"
 
-  # READ
-  get "/images", :controller => "images", :action => "index"
-  get "/images/:id", :controller => "images", :action => "show"
+  # # READ
+  # get "/images", :controller => "images", :action => "index"
+  # get "/images/:id", :controller => "images", :action => "show"
 
-  # UPDATE
-  get "/images/:id/edit", :controller => "images", :action => "edit"
-  post "/update_image/:id", :controller => "images", :action => "update"
+  # # UPDATE
+  # get "/images/:id/edit", :controller => "images", :action => "edit"
+  # post "/update_image/:id", :controller => "images", :action => "update"
 
-  # DELETE
-  get "/delete_image/:id", :controller => "images", :action => "destroy"
+  # # DELETE
+  # get "/delete_image/:id", :controller => "images", :action => "destroy"
   #------------------------------
 
   # Routes for the Survey resource:
@@ -22,21 +22,21 @@ Rails.application.routes.draw do
   get "/surveys/new", :controller => "surveys", :action => "new"
   post "/create_survey", :controller => "surveys", :action => "create"
 
-  # READ
-  get "/surveys", :controller => "surveys", :action => "index"
-  get "/surveys/:id", :controller => "surveys", :action => "show"
+  # # READ
+  # get "/surveys", :controller => "surveys", :action => "index"
+  # get "/surveys/:id", :controller => "surveys", :action => "show"
 
-  # UPDATE
-  get "/surveys/:id/edit", :controller => "surveys", :action => "edit"
-  post "/update_survey/:id", :controller => "surveys", :action => "update"
+  # # UPDATE
+  # get "/surveys/:id/edit", :controller => "surveys", :action => "edit"
+  # post "/update_survey/:id", :controller => "surveys", :action => "update"
 
-  # DELETE
-  get "/delete_survey/:id", :controller => "surveys", :action => "destroy"
-  #------------------------------
+  # # DELETE
+  # get "/delete_survey/:id", :controller => "surveys", :action => "destroy"
+  # #------------------------------
 
-  get 'sessions/create'
+  # get 'sessions/create'
 
-  get 'sessions/destroy'
+  # get 'sessions/destroy'
 
   devise_for :users, controllers: { registrations: "registrations" }
 
@@ -46,9 +46,6 @@ get "/beta", :controller => "home", :action => "beta"
 
 # Routes for the About resource
 get "/aboutus", :controller => "about", :action => "index"
-
-# Routes for the Lifestyles resource
-get "/lifestyle", :controller => "lifestyles", :action => "index"
 
 # Routes for the Footprints resource
 get "/footprint", :controller => "footprints", :action => "index"
@@ -61,7 +58,10 @@ get "/trip", :controller => "trips", :action => "index"
 
 get "/trip/map", :controller => "trips", :action => "map"
 
-get "/trip/google_map", :controller => "trips", :action => "google_map"
+# get "/trip/google_map", :controller => "trips", :action => "google_map"
+
+# Routes for the Users resource
+get "/users", :controller => "users", :action => "index"
 
 # Routes for the Stripe Charges resource
 resources :charges
